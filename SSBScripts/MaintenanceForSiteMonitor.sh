e0bed351f66523744a80724d1d0fe9c37e4fb69a
Stefano Belforte <belforte@cern.ch>
2010-12-20 13:24:53 +0000

    adapt to GOCDB-4

--- SSBScripts/MaintenanceForSiteMonitor.sh
+++ SSBScripts/MaintenanceForSiteMonitor.sh
@@ -22,25 +22,29 @@ cat <<EOF > $outFile
 EOF
 
 timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+gocBaseUrlBegin="https://next.gocdb.eu/portal/index.php?Page_Type=View_Object&object_id="
+gocBaseUrlEnd="&grid_id=0"
 
 #
 #
-cmsSites=`curl -ks https://cmsweb.cern.ch/sitedb/sitedb/json/index/SitetoCMSName?name= | tr "," "\n"|cut -d"'" -f6|grep -v "T1_CH_CERN"|grep -v "T2_CH_CAF"|sort -u`
+cmsSites=`/usr/bin/curl -ks https://cmsweb.cern.ch/sitedb/sitedb/json/index/SitetoCMSName?name= | tr "," "\n"|cut -d"'" -f6|grep -v "T1_CH_CERN"|grep -v "T2_CH_CAF"|sort -u`
 
 for s in $cmsSites
 do
-  sam=`curl -ks https://cmsweb.cern.ch/sitedb/sitedb/json/index/CMStoSAMName?name=$s | tr "," "\n"|cut -d"'" -f6 | head -1`
-  goc=`curl -s http://goc.grid.sinica.edu.tw/gstat/$sam/|grep "goc.gridops.org"| cut -d= -f3,4|cut -d">" -f1|tr -d '"'`
+  sam=`/usr/bin/curl -ks https://cmsweb.cern.ch/sitedb/sitedb/json/index/CMStoSAMName?name=$s | tr "," "\n"|cut -d"'" -f6 | head -1`
+  echo $s $sam
+  goc=`/usr/bin/curl -ks "https://goc.gridops.org/gocdbpi/public/?method=get_site_list&sitename=$sam"|tr ' ' '\n'|grep PRIMARY_KEY|cut -d'"' -f2|sed s/G0//`
+  gocUrl=$gocBaseUrlBegin$goc$gocBaseUrlEnd
 
-  if [ "$s" == "T1_US_FNAL" ]
-  then
-    goc=""
-  fi
+#  if [ "$s" == "T1_US_FNAL" ]
+#  then
+#    goc=""
+#  fi
 
   if [ "$goc" != "" ]
   then
       value="GOCDB"
-      link=$goc"#downtimes"
+      link=$gocUrl
   else
       value="OIM"
       link="http://tinyurl.com/nv66gk"
@@ -48,11 +52,11 @@ do
       #link="http://oim.grid.iu.edu/pub/maintenance/show.php"
   fi
 
-  if [ "$s" == "T2_FI_HIP" ]
-  then
-      value="GOCDB"
-      link="https://goc.gridops.org/site/list?id=322#downtimes"
-  fi
+#  if [ "$s" == "T2_FI_HIP" ]
+#  then
+#      value="GOCDB"
+#      link="https://goc.gridops.org/site/list?id=322#downtimes"
+#  fi
 
   if [ "$s" != "T2_FR_GRIF_LAL" ]
   then
