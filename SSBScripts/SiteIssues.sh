7438afe6253dbc18418723e050d3c6e443c4578a
Andrea Sciaba <samcms@cern.ch>
2011-06-09 15:48:52 +0000

    Changed path to SiteIssues.py

--- SSBScripts/SiteIssues.sh
+++ SSBScripts/SiteIssues.sh
@@ -2,7 +2,7 @@
 cd /afs/cern.ch/cms/LCG/SiteComm/SiteIssues
 mkdir -p tmp
 cd tmp
-/usr/bin/python /afs/cern.ch/user/b/belforte/SiteMonitor/SiteIssues.py
+/usr/bin/python /afs/cern.ch/user/s/samcms/COMP/SITECOMM/SSBScripts/SiteIssues.py
 cd ..
 mv tmp/* .
 rmdir tmp
