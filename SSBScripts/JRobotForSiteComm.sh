c9d7dc97983ec24a47d1052454c22ba76f540bbb
Andrea Sciaba <Andrea.Sciaba@cern.ch>
2013-07-24 12:20:49 +0200

    Removed obsolete JobRobot scripts

--- SSBScripts/JRobotForSiteComm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-OUTDIR=/afs/cern.ch/cms/LCG/SiteComm
-LOGDIR=$OUTDIR/log
-tmpfile=`mktemp`
-dir=`dirname $0`
-$dir/sites.pl > $tmpfile
-$dir/JRobotForSiteComm.py $OUTDIR/JobRobotForSiteComm.txt $tmpfile >> $LOGDIR/JRobotForSiteComm.log 2>&1
-rm -f $tmpfile
