cc992c39d88568e63dc8affdb7c7f0b02d3a71ad
Andrea Sciaba <Andrea.Sciaba@cern.ch>
2013-10-24 12:03:59 +0200

    Using SiteDBv2 API

--- SSBScripts/sites.pl
+++ SSBScripts/sites.pl
@@ -4,8 +4,8 @@
 #
 #use strict;
 
-use LWP::Simple;
-use XML::Parser;
+use LWP::UserAgent;
+use JSON;
 
 package Site;
 
@@ -21,17 +21,55 @@ sub new {
 
 package main;
 
+# Modify as needed
+my $email = 'Andrea.Sciaba@cern.ch';
+
+# Define HTTPS environment to use proxy
+$ENV{HTTPS_CA_DIR} = (defined $ENV{X509_CERT_DIR})?$ENV{X509_CERT_DIR}:"/etc/grid-security/certificates";
+my $GSIPROXY = (defined $ENV{X509_USER_PROXY})?$ENV{X509_USER_PROXY}:"/tmp/x509up_u$<";
+$ENV{HTTPS_CA_FILE} = $GSIPROXY;
+$ENV{HTTPS_CERT_FILE} = $GSIPROXY;
+$ENV{HTTPS_KEY_FILE}  = $GSIPROXY;
+
 # Array with all Sites and tiers
 
-#Get XML file from SiteDB
+#Get JSON file from SiteDB
+my $url = "https://cmsweb.cern.ch/sitedb/data/prod/site-names";
 
-my $url = "https://cmsweb.cern.ch/sitedb/reports/showXMLReport?reportid=naming_convention.ini";
-my $doc = get($url) or die "Cannot retrieve XML\n";
+# Set header to select output format
+$header = HTTP::Headers->new(
+			     Accept => 'application/json');
 
-# Parse XML
+$ua = LWP::UserAgent->new(
+			  from => $email);
+$ua->default_headers($header);
 
-$p = new XML::Parser(Handlers => {Char  => \&h_char_names});
-$p->parse($doc) or die "Cannot parse XML\n";
+my $response = $ua->get($url) or die "Cannot retrieve XML\n";
+#print $response->decoded_content;
+
+#Parse JSON
+my $ref = from_json($response->decoded_content);
+
+my %site2cms = ();
+my %site2lcg = ();
+
+foreach my $item (@{$ref->{'result'}}) {
+    my $type = $item->[0];
+    my $site_name = $item->[1];
+    my $alias = $item->[2];
+
+    if ($type eq 'cms') {
+	$site2cms{$site_name} = $alias;
+    } elsif ($type eq 'lcg') {
+	$site2lcg{$site_name} = $alias;
+    }
+}
+
+foreach (keys %site2cms) {
+    print $site2cms{$_} . "\t" . $site2lcg{$_} . "\n";
+}
+
+exit 0;
 
 print map("$_\n", sort @sites);
 
