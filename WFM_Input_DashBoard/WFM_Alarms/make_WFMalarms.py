69f329cb980161bddb3096686331ea0b8a03d681
Mohd Adli <doliquest@gmail.com>
2013-12-03 17:29:17 +0100

    Update make_WFMalarms.py

--- WFM_Input_DashBoard/WFM_Alarms/make_WFMalarms.py
+++ WFM_Input_DashBoard/WFM_Alarms/make_WFMalarms.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 import db_Initialize, db_ExtractStatusEntry, db_time_converter
 import shutil, time, urllib, urllib2
@@ -64,16 +63,16 @@ for site in sites:
     if pledge_SafeDivision == 0:
         pledge_SafeDivision=1
 
-    # fetching site information numbers of the last 24 hours 
+    # fetching site information numbers of the last 24 hours
     tmp_csv_merg="temp_site_RunAndPend.csv"
     #RUNNING
-    url_filledwithsite=("http://dashb-cms-prod.cern.ch/dashboard/request.py/"         
+    url_filledwithsite=("http://dashb-cms-prod.cern.ch/dashboard/request.py/"
                     "condorjobnumberscsv?sites="+site+"&sitesSort=1&"
                     "jobTypes=&start=null&end=null&timeRange=last24&"
                     "granularity=15%20Min&sortBy=3&series=All&type=r")
 
     print url_filledwithsite
-    #get content from url_filledwithsite to tmp_cvs_run    
+    #get content from url_filledwithsite to tmp_cvs_run
     tmp_csv_run= urllib2.urlopen(url_filledwithsite).read()
     print 'fetched running csv of',site, 'succesfully'
 
@@ -85,7 +84,7 @@ for site in sites:
     print url_filledwithsite
     tmp_csv_pen= urllib2.urlopen(url_filledwithsite).read()
     print 'fetched pending csv of',site, 'succesfully'
-    
+
 
     #MERGE the TWO list togther with paste and put a "," between the two instead of an endline symbol
     #tmp_csv_merg = [run+','+pen for run,pen in zip(tmp_csv_run.splitlines(),tmp_csv_run.splitlines())]
@@ -103,7 +102,7 @@ for site in sites:
     nb_Pen_Prod="-1"
     #read each line
     for line in tmp_csv_merg:
-        args = line.split(',')      
+        args = line.split(',')
         jobtype = args[2]
         if jobtype == 'Clean':
             nb_Run_Clean =  args[0]
@@ -124,7 +123,7 @@ for site in sites:
             nb_Run_Prod =  args[0]
             nb_Pen_Prod = args[4]
             #this is also the last of the 5 variables that I have to read, so we write away our set and reset all the others
-            nb_SUMRun = nb_Run_Clean + nb_Run_Log + nb_Run_Merge  + nb_Run_RelVal + nb_Run_Proc +nb_Run_Prod 
+            nb_SUMRun = nb_Run_Clean + nb_Run_Log + nb_Run_Merge  + nb_Run_RelVal + nb_Run_Proc +nb_Run_Prod
             nb_SUMPen = nb_Pen_Clean + nb_Pen_Log + nb_Pen_Merge + nb_Pen_RelVal + nb_Pen_Proc + nb_Pen_Prod
             #get time and transform format
             timePoint=strptime(args[1],"%d-%b-%y %H:%M:%S")
@@ -132,7 +131,7 @@ for site in sites:
 
             # fetching the status number per time_point
             status= db_ExtractStatusEntry.getSiteStatusEntry( site, timePoint ,statusjson )
-           
+
             Ratio = float(SUMRun)/pledge_SafeDivision
             PerfectRatio=1
             SeventyRatio=0.7
@@ -140,17 +139,25 @@ for site in sites:
             GL_AL=Condition and (SUMPen>=10) and (SUMRun==0)
             GL_UNDEF= not Condition
             GL_OK=(GL_AL==False) or (GL_UNDEF==False)
-            A_WARN=Condition and (Ratio<th_war) and (Ratio>=th_al) and (SUMPen>10)
+            A_WARN=Condition and (Ratio<th_war) and (Ratio>=th_al)&&(SUMPen>10)
             A_AL=Condition and (Ratio<th_al) and (SUMPen>10)
             A_UNDEF=(not Condition)
             A_OK=(A_AL==0) and (A_WARN==0) and (A_UNDEF==0)
-
-
+            SPEC_Cond=Condition and (SUMPen>=10)
+            if SPEC_Cond:
+                SPEC_SUMRun=SUMRun
+            else:
+                SPEC_SUMRun=0
+            if SPEC_Cond:
+                SPEC_Pledge=pledge_SafeDivision
+            else:
+                SPEC_Pledge=0
             
-             #format of data.dat: dateTime State SUMRun SUMPen Pledge Ratio 1.0 0.7 ALARM_OK ALARM_WARNING ALARM_ALARM ALARM_UNDEFINED GLIDEIN_OK GLIDEIN_ALARM GLIDEIN_UNDEF SPEC_SUMRun SPEC_Pledge SPEC_CondCanBeRemoved 
+
+             #format of data.dat: dateTime State SUMRun SUMPen Pledge Ratio 1.0 0.7 ALARM_OK ALARM_WARNING ALARM_ALARM ALARM_UNDEFINED GLIDEIN_OK GLIDEIN_ALARM GLIDEIN_UNDEF SPEC_SUMRun SPEC_Pledge SPEC_CondCanBeRemoved
              #store int tmp_all
             tmp_all.append (timePoint + ' ' + status + ' ' + nb_SUMRun + ' ' + nb_SUMPen +
-                    ' ' + pledge_SafeDivision + ' ' + Ratio + ' ' + PerfectRatio + 
+                    ' ' + pledge_SafeDivision + ' ' + Ratio + ' ' + PerfectRatio +
                     ' ' + A_OK+ ' ' +A_WARN,A_AL+ ' ' +A_UNDEF+ ' ' +GL_OK+
                     ' ' + GL_AL+ ' ' +GL_UNDEF+ ' ' +SPEC_SUMRun+ ' ' +SPEC_Pledge +
                     ' '  +SPEC_Cond)
@@ -163,3 +170,4 @@ GlideInAlarm = {}
 for nb_entries in [1, 4, 32, 96]:
     #get last nb_entrues in tmp_all
     part_dat = tmp_all[-nb_entries:]
+
