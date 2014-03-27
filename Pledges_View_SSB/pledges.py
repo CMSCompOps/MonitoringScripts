42d51937c73d540ccdf3411d9709c12f29843662
Gökhan <gokhan.kandemir@cern.ch>
2014-03-27 18:40:36 +0100

    update script \t

--- Pledges_View_SSB/pledges.py
+++ Pledges_View_SSB/pledges.py
@@ -96,7 +96,7 @@ def savetoFile(pledges, year,outputfile_txt):
       if (pledges[tmpPledges]      == 0)     : color = 'yellow'
       if (pledges[tmpPledges]      > 0)      : color = 'green'
       if (str(pledges[tmpPledges]) == 'n/a') : color = 'white'
-      fileOp.write(saveTime + " " + tmpPledges + "\t" + str(pledges[tmpPledges]) + "\t" + color + "\t" +  url + "\n" )
+      fileOp.write(saveTime + "\t" + tmpPledges + "\t" + str(pledges[tmpPledges]) + "\t" + color + "\t" +  url + "\n" )
 
   fileOp.close()
 
