f62c753a66167753e91ab1ec1bf61ab81c28bb37
Gökhan Kandemir <gokhan.kandemir@cern.ch>
2014-04-06 23:06:23 +0200

    prodcore script

---
+++ Pledges_View_SSB/prod_cores/prodcores.py
@@ -0,0 +1,57 @@
+import urllib2
+import json, time, string
+
+def getDatafromURL():
+	url='http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=136&time=24&dateFrom=&dateTo=&site=T0_CH_CERN&sites=all&clouds=undefined&batch=1';
+	print "Getting the url %s" % url
+	obj = urllib2.urlopen(url)
+	data = obj.read()
+	rows = json.loads(data)
+	return rows
+
+def calculateProdCore(json):
+	saveTime = time.strftime('%Y-%m-%d %H:%M:%S')
+	urll = "mailto:cms-comp-ops-site-support-team@cern.ch?subject=Pledges_View_SSB"
+	filename = "prod"
+	jsonCode = '{ "prodCores":['
+	jsonCodeEnd = ']}'
+	f = open(filename + ".txt", "w")
+	j = open(filename + ".json", "w")
+	for row in json['csvdata']:
+		tierName = row['VOName'][0:2]
+		siteName = row['VOName']
+		value = row['Value']
+		status = row['Status']
+		color = row['COLORNAME']
+		#_______________Calculate Prod[Core]____________________________
+		if tierName != 'T0':
+			if (tierName == 'T1') or (tierName == 'T3' or siteName == 'T2_CH_CERN_AI' or siteName == 'T2_CH_CERN_HLT') :
+				if color == 'white': # value = n/a ?
+					prodCore = 'n/a'
+				else:
+					if status == value :
+						prodCore = value
+					else: 
+						prodCore = status
+			elif(tierName == 'T2'):
+				if color == 'white':
+					prodCore = 'n/a'
+				else:
+					if status == value :
+						prodCore = value
+					else: 
+						prodCore = int(status) * 0.5
+			if str(prodCore).find('.') >= 0 : prodCore = str(prodCore)[0:str(prodCore).find('.')]
+			jsonCode = jsonCode + "{" + '"siteName":"' + siteName + '", "prodCore":' + prodCore + ',' + '"color":"' + color + '",' + '"url":"' + urll + '"},'    
+			f.write(saveTime + "\t" + siteName + "\t" + prodCore + "\t" + color + "\t" + urll + "\n")
+
+			print saveTime, siteName, prodCore, color, urll
+	#____________________save to JSON file______________________________
+	jsonCode = (jsonCode + jsonCodeEnd).replace("},]}", "}]}")
+	j.write(jsonCode)
+	#___________________________________________________________________
+	#_______________________________________________________________
+
+if __name__ == '__main__':
+	rows = getDatafromURL() #get all pledge from dashBoard
+	calculateProdCore(rows)
---
+++ Pledges_View_SSB/prod_cores/run_prodcores.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# written by Gökhan Kandemir => gokhan.kandemir@cern.ch
+# outputfile [year]_pledges.txt
+# outputfile [year]_pledges.json
+# outputfile [year]_pledges.html
+# usercert and userkey files must be in folder "data"
+# this script fetchs all pledges values and matchs with siteName from siteDB.
+clear
+# Email if things are running slowly
+if [ -f scriptRunning.run ];
+then
+   echo "run_prodcores.sh is already running. Will send an email to the admin."
+   # script to send simple email
+   # email subject
+   SUBJECT="[Prod[Cores]] load Prod[Cores]"
+   # Email To ?
+   EMAIL="gokhan.kandemir@cern.ch"
+   # Email text/message
+   if [ -f emailmessage.txt ];
+   then
+      rm emailmessage.txt
+   fi
+   touch emailmessage.txt
+   EMAILMESSAGE="/tmp/emailmessage.txt"
+   echo "run_prodcores.sh  is running to slowly."
+   # send an email using /bin/mail
+   /bin/mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
+
+else
+     echo "bash run_prodcores.sh succesfully"
+     touch scriptRunning.run
+fi
+
+
+#Run the script
+txt="prod"
+echo "python prodcores.py"
+python prodcores.py &> prod.log
+
+problem="$?"
+echo "problem: $problem"
+echo "The files were created succesfully."
+
+cp $txt".txt"  /afs/cern.ch/user/c/cmst1/www/SST
+cp $txt".json"  /afs/cern.ch/user/c/cmst1/www/SST
+
+rm scriptRunning.run
+
