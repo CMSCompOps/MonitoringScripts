a9cdbb7fe551bf220a2057a1891b85b1a57ed91a
Gökhan Kandemir <gokhan.kandemir@cern.ch>
2014-05-27 17:14:52 +0200

    update for siteDB v2

--- SR_View_SSB/WRControl/WaitingRoom_Sites.py
+++ SR_View_SSB/WRControl/WaitingRoom_Sites.py
@@ -9,36 +9,38 @@ import string
 import urllib, httplib, re
 import pickle 
 
-#extract nonwaitingroommsites
+#extract nonwaitingroommsites from ActiveSites script output
 url2 = "https://cmsdoc.cern.ch/cms/LCG/SiteComm/T2WaitingList/WasCommissionedT2ForSiteMonitor.txt"
 
 # function needed to fetch a list of all sites from siteDB
 def fetch_all_sites(url,api):
-  # examples in:
+  # example in
   # https://github.com/gutsche/old-scripts/blob/master/SiteDB/extract_site_executive_mail_addresses.py
   # https://github.com/dballesteros7/dev-scripts/blob/master/src/reqmgr/input-blocks.py
   # https://twiki.cern.ch/twiki/bin/view/CMSPublic/CompOpsWorkflowOperationsWMAgentScripts#Resubmit
+
   headers = {"Accept": "application/json"}
   if 'X509_USER_PROXY' in os.environ:
-      print 'X509_USER_PROXY found'
-      conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
+    print 'X509_USER_PROXY found'
+    conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_PROXY'), key_file = os.getenv('X509_USER_PROXY'))
   elif 'X509_USER_CERT' in os.environ and 'X509_USER_KEY' in os.environ:
-      print 'X509_USER_CERT and X509_USER_KEY found'
-      conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_CERT'), key_file = os.getenv('X509_USER_KEY'))
+    print 'X509_USER_CERT and X509_USER_KEY found'
+    conn = httplib.HTTPSConnection(url, cert_file = os.getenv('X509_USER_CERT'), key_file = os.getenv('X509_USER_KEY'))
+  elif os.path.isfile('/data/certs/servicecert.pem') and os.path.isfile('/data/certs/servicekey.pem'):
+    conn = httplib.HTTPSConnection(url, cert_file = '/data/certs/servicecert.pem', key_file = '/data/certs/servicekey.pem')
   else:
-      print 'You need a valid proxy or cert/key files'
-      sys.exit()
+    print 'You need a valid proxy or cert/key files'
+    sys.exit()
   print 'conn found in if else structure'
   r1=conn.request("GET",api, None, headers)
   print 'r1 passed'
   r2=conn.getresponse()
-  print 'r2 passed'
+  print 'r1 passed'
   inputjson=r2.read()
-  print '-------------------------------------------------------------'
-  #print inputjson
   result = simplejson.loads(inputjson)
   conn.close()
   return result
+
 
 def getNonWaitingRoomSites(url):
     tmp_sites = []
@@ -66,31 +68,22 @@ def main_function(outputfile_txt):
 
   # all sites
   print 'starting to fetch all sites from siteDB'
-  jn = fetch_all_sites('cmsweb.cern.ch','/sitedb/data/prod/site-names') 
+  jn = fetch_all_sites('cmsweb.cern.ch','/sitedb/data/prod/site-names')
+
   #json form:
   #{"desc": {"columns": ["type", "site_name", "alias"]}, 
   # "result": [
   #             ["cms", "ASGC", "T1_TW_ASGC"],
   #             ["cms", "BY-NCPHEP", "T3_BY_NCPHEP"]
   #            ...
-  
-  # match the information
-  #print jn['result']
-  #site_T1= []
-  site_T2= []
-  #site_T3= []
-  #print 'printing i'
-  for i in jn['result']:
-    if i[jn['desc']['columns'].index('type')]=='cms':
-      sitedbname=i[jn['desc']['columns'].index('alias')]
-      #print sitedbname
-      #if 'T1' in sitedbname: 
-      #  site_T1.append(sitedbname)
-      if 'T2' in sitedbname:
-        site_T2.append(sitedbname)
-      #if 'T3' in sitedbname: 
-      #   site_T3.append(sitedbname)
-  #print site_T2
+
+ #__________________siteDB v2_________________________
+ 
+  site_T2 = []
+  for row in jn['result']:
+    if (row[0] == 'cms') and (row[2][0:2] == 'T2'):
+      site_T2.append(row[2])
+ #____________________________________________________ 
   print '--------------------------------------------------------'
   print 'Sites in waiting room:'
   waitingRoom_sites = [ site for site in site_T2 if not site in nonWaitingRoom_Sites]
@@ -99,13 +92,12 @@ def main_function(outputfile_txt):
   f1=open('./'+outputfile_txt, 'w+')
   now_write=(datetime.utcnow()).strftime("%Y-%m-%d %H:%M:%S")
 
-
   # write file that can be loaded in SSB
   f1.write('# This txt goes into SSB and marks sites red when the site is in the waiting room:\n')
   f1.write('# Readme:\n# https://raw.githubusercontent.com/CMSCompOps/MonitoringScripts/master/SR_View_SSB/WRControl/Readme.txt\n')
 
   print "Local current time :", now_write
-  link = "https://cmst1.web.cern.ch/CMST1/WFMon/WaitingRoom_Sites.txt"
+  link = "https://dashb-ssb.cern.ch/dashboard/request.py/sitereadinessrank?columnid=45#time=2184&start_date=&end_date=&sites=T0/1/2"
   for k in waitingRoom_sites:
     print k, 'in', 'red'
     f1.write(now_write+'\t')        # timestamp
