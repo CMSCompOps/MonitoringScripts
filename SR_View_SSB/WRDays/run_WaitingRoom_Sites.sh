a1253d47e5b6c4e7c638960b0d528eed1473c4a9
John P. Artieda <artiedaj@fnal.gov>
2014-06-23 14:58:07 -0500

    change of outputdir for WRDays

--- SR_View_SSB/WRDays/run_WaitingRoom_Sites.sh
+++ SR_View_SSB/WRDays/run_WaitingRoom_Sites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 location="/afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/WRDays/"
-outputdir="afs/cern.ch/user/c/cmst1/www/WFMon/"
+outputdir="/afs/cern.ch/user/c/cmst1/www/SST/"
 cd $location
 
 if [ -f scriptRunning.run ];
