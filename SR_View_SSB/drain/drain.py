63fb379d9999c7e83bf87f656ce7795007d68f49
Gökhan Kandemir <gokhan.kandemir@cern.ch>
2014-07-11 19:29:34 +0200

    new drain metric

--- SR_View_SSB/drain/drain.py
+++ SR_View_SSB/drain/drain.py
@@ -5,9 +5,24 @@ import time
 from datetime import datetime
 from datetime import timedelta
 from datetime import date
-#_______________________________________________________________________
 
-def extractJson(url):
+urlDrain  = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=158&time=24&dateFrom=&dateTo=&site=T0_CH_CERN&sites=all&clouds=undefined&batch=1"
+urlWr     = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=153&time=24&dateFrom=&dateTo=&site=T2_AT_Vienna&sites=all&clouds=undefined&batch=1"
+urlSR     = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=45&time=168&dateFrom=&dateTo=&site=T1_DE_KIT&sites=all&clouds=undefined&batch=1"
+urlSD     = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=121&time=48&dateFrom=&dateTo=&site=T0_CH_CERN&sites=all&clouds=undefined&batch=1"
+urlMorgue = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=199&time=24&dateFrom=&dateTo=&site=T2_AT_Vienna&sites=all&clouds=undefined&batch=1"
+
+daysBrown    = {}  # keeps down days count
+daysGreen    = {}  # keeps ready days count
+daysWhite    = {}  # keeps noinfo days count
+tmpDrainList = []  # keeps Temporary Drain List after condition process
+tmpDownList  = []  # keeps Temporary Down List after condition process
+newDrainList = {}  # keeps new Drain List
+
+
+#______________________function returns json code from url________________________
+
+def extractJson(url): 
   print "Getting the url %s" % url
   request = urllib2.Request(url, headers = {"Accept":"application/json"})
   response = urllib2.urlopen(request)
@@ -15,9 +30,9 @@ def extractJson(url):
   rows = simplejson.loads(data)
   return rows
 
-#_______________________________________________________________________
+#_________________function returns siteList by using your parameter________________
 
-def getList(url, status):
+def getList(url, status): # status could have in, down, drain, SD, *
     rows = extractJson(url)
     site_list = []
     for row in rows['csvdata']:
@@ -29,78 +44,90 @@ def getList(url, status):
                 if not row["VOName"] in site_list: site_list.append(row["VOName"])
     return site_list
 
-#_______________________________________________________________________
+#_____________________________________________________________________________________________________
 
-def calculateDays(url, status):
-  enddate  = date.today()
-  value = datetime.fromtimestamp(time.time() - 7 * 24 * 60 * 60)
+#_________________________the function calculates day counts in last week_____________________________
+
+  # In this algorithm, startdate and enddate are search borders. Script finds the records which are between startdate and enddate.
+  
+  #       <-----------------------7 days----------------------->
+  #       |----------------------------------------------------|
+  #   startdate                                          enddate(today)
+  
+  # Because records have date and time information, these informations are called starttime and endtime
+
+def getDayCounts(url, status):
+  #________________________________definition of time and variable___________________________________
+
+  enddate  = date.today()                                                               # gives today date. 
+  value = datetime.fromtimestamp(time.time() - 7 * 24 * 60 * 60)                        # to get 7 days ago from today
   timeStamp = value.strftime('%Y-%m-%d').split("-")
-  startdate = date(int(timeStamp[0]), int(timeStamp[1]), int(timeStamp[2]))
-  print "End Date : ", enddate
-  print "Start Date : ", startdate
-  jsn=extractJson(url) # results from dashboard extracts into JSON file
-  allSites = getList(url, "*")
-  # sum @ days 
-  days_per_site={}
-  for site in allSites: # Read all sites from allsites variable
-    wrDays = 0         # keeps number of wrDays per Site
-    days  = 0             # keeps temporary wrDays
-    temp = 0  
-    for k in jsn['csvdata']: # JSON file reads.
-      if k['VOName'] != site: continue
-#************************************************Modifications**************************************
-      elif k['COLORNAME'] == status: # if the site is red then calculate wrDays
-      	starttime = datetime(*(time.strptime( k['Time'] ,'%Y-%m-%dT%H:%M:%S')[0:6])) # starttime from JSON file
-      	endtime   = datetime(*(time.strptime( k['EndTime'] ,'%Y-%m-%dT%H:%M:%S')[0:6])) # endtime from JSON file
+  startdate = date(int(timeStamp[0]), int(timeStamp[1]), int(timeStamp[2]))      		# 7 days ago from today
+  jsn=extractJson(url)                                                                  # results from dashboard extracts into JSON file
+  allSites = getList(url, "*")                                                          # gets allsite list from getList function
+  days_per_site={} 																		# keeps all siteNames and its day informations
+
+  #_______________________________________________day calculation process_______________________________________________________
+  
+  for site in allSites:
+    daysCount = 0 # keeps totalday count
+    tmp_days  = 0 # keeps temp day count
+    temp 	  = 0 # keeps endtime
+    for row in jsn['csvdata']:
+      if row['VOName'] != site: continue
+      elif row['COLORNAME'] == status: 
+      	starttime = datetime(*(time.strptime( row['Time'] ,'%Y-%m-%dT%H:%M:%S')[0:6]))       # starttime of record which comes JSON
+      	endtime   = datetime(*(time.strptime( row['EndTime'] ,'%Y-%m-%dT%H:%M:%S')[0:6]))    # endtime of record which comes JSON
       	if startdate > starttime.date():
       		if startdate > endtime.date(): continue
       		elif startdate < endtime.date():
       			if startdate > starttime.date():
-      				days = endtime.date() - startdate
+      				tmp_days = endtime.date() - startdate
       			elif startdate <= starttime.date():
-      				days = endtime.date() - starttime.date()
-      			temp = days.days
-			if endtime.date() > enddate: temp = temp - 1
-			wrDays+= temp
+      				tmp_days = endtime.date() - starttime.date()
+      			temp = tmp_days.days
+			if endtime.date() > enddate: temp = temp - 1                                     # I can use "tmp_days -= 1" but When I used this I encountered "readonly attribute error"
+			daysCount+= temp
       	elif startdate <= starttime.date():
-      		days = endtime.date() - starttime.date()
-		temp = days.days
-		if endtime.date() > enddate: temp = temp - 1
-      	wrDays+= temp
-      	days_per_site[site] = wrDays
-    if wrDays == 0:
+      		tmp_days = endtime.date() - starttime.date()
+		temp = tmp_days.days
+		if endtime.date() > enddate: temp = temp - 1                                         # I can use "tmp_days -= 1" but When I used this I encountered "readonly attribute error"
+      	daysCount+= temp
+      	days_per_site[site] = daysCount
+    if daysCount == 0:
         days_per_site[site] = 0
-  return days_per_site
-#_______________________________________________________________________
 
-def calculateSR(allSites, daysGreen, daysYellow, daysBrown, daysWhite):
-    average_per_site = {}
-    readiness = 0.0
+  return days_per_site
+#___________________________the function calculates Site Readiness Rate per site ___________________
+
+# SiteReadiness = (Ready(G) + Warning(Y)) / (totalDays - scheduleDowntime(B) - NoInfo(W))
+# but because this metric is so sensitive, Warning days can be ignored. We need perfect readiness
+
+def CalSiteReadRate(allSites, daysGreen, daysBrown, daysWhite):
+    average_per_site = {}                                                           # keeps all siteNames and their site readiness rate
     for site in allSites:
-        if (site in daysGreen) and (site in daysYellow) and (site in daysBrown) and (site in daysWhite) :  
+        if (site in daysGreen) and (site in daysBrown) and (site in daysWhite) :  
             green  = daysGreen[site]
-            yellow = daysYellow[site]
-            brown = daysBrown[site]
-            white = daysWhite[site]
+            brown  = daysBrown[site]
+            white  = daysWhite[site]
             readiness = 0.0
-            if brown != 7 :
-                readiness = (green + yellow) / float(7 - brown - white)
+            if brown != 7 :                                                         # if brown = 7 => script encounters "ZeroDivisionError" 
+                readiness = (green) / float(7 - brown - white) 
             average_per_site[site] = readiness
     return average_per_site
 
-#_______________________________________________________________________
+#__________________________________________________________________________________________________
 
 def writeFile(siteList):
     saveTime = time.strftime('%Y-%m-%d %H:%M:%S')
     filename = "drain"
-    path = ""
-    #path = "/afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/drain/"
+    path = "/afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/drain/"
     url = "https://cmst1.web.cern.ch/CMST1/SST/drain.txt"
     f = open(path + filename + ".txt", "w")
     for rows in siteList:
         color = "green"
         if siteList[rows] == "drain" : color = "yellow"
-        if siteList[rows] == "down" : color = "brown"
+        if siteList[rows] == "down" : color  = "red"
         f.write(saveTime + "\t" + rows + "\t" + siteList[rows] + "\t" + color + "\t" + url + "\n")
     print "List has been created successfully"
 
@@ -108,84 +135,57 @@ def writeFile(siteList):
 #________________________________________________________________________
 
 
-# run program for last month, last 2 months and last 3 months
-if __name__ == '__main__':
-    urlDrain     = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=158&time=24&dateFrom=&dateTo=&site=T0_CH_CERN&sites=all&clouds=undefined&batch=1"
-    urlWr         = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=153&time=24&dateFrom=&dateTo=&site=T2_AT_Vienna&sites=all&clouds=undefined&batch=1"
-    urlSR         = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=45&time=168&dateFrom=&dateTo=&site=T1_DE_KIT&sites=all&clouds=undefined&batch=1"
-    urlSD         = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=45&time=24&dateFrom=&dateTo=&site=T1_DE_KIT&sites=all&clouds=undefined&batch=1"
-    urlMorgue  = "http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=199&time=24&dateFrom=&dateTo=&site=T2_AT_Vienna&sites=all&clouds=undefined&batch=1"
-
-    daysBrown = {}
-    daysYellow = {}
-    daysGreen = {}
-    daysWhite  = {}
-    newDrainList = []
-    newDownList = []
-    newList = {}
-    
+def getAllInformation():
     #________________________________getting oldDrainList, oldDownList, wr list, morgue list, sr=SD List, full site list__________________________
+
+    fullSiteList  = getList(urlDrain, "*")                                                        # gets full site list from metric 158
+    wrList        = getList(urlWr, "in")                                                          # gets current waiting room list from metric 153
+    morgueList    = getList(urlMorgue, "in")                                                      # gets current morgue list from metric 199
+    oldDownList   = getList(urlDrain, "down")                                                     # gets old down list from metric 158 
+    oldDrainList  = getList(urlDrain, "drain")                                                    # gets old drain list from metric 158
+    srStatusList  = getList(urlSD, "OUTAGE SCHEDULED 1/1 SRMv2 down, 1/1 CREAM-CE down")          # gets site readiness status from metric 121
     
-    fullSiteList       = getList(urlDrain, "*")
-    wrList              = getList(urlWr, "in")
-    morgueList      = getList(urlMorgue, "in")
-    oldDownList    = getList(urlDrain, "down")
-    oldDrainList     = getList(urlDrain, "drain")
-    srStatusList     = getList(urlSD, "SD")
+    print 'starting to fetch all sites from DashBoard'
+    daysBrown  = getDayCounts(urlSR, "brown")
+    daysWhite  = getDayCounts(urlSR, "white")
+    daysGreen  = getDayCounts(urlSR, "green")
     
-    print 'starting to fetch all sites from siteDB'
-    daysBrown     = calculateDays(urlSR, "brown")
-    daysYellow = calculateDays(urlSR, "yellow")
-    daysWhite  = calculateDays(urlSR, "white")
-    daysGreen = calculateDays(urlSR, "green")
+    #_______________________________ calculates site readiness ranking for last week per site. __________________________
     
-    #_______________________________calculates site readiness ranking for last week per site._____________________________________________
+    average_per_site = CalSiteReadRate(fullSiteList, daysGreen, daysBrown, daysWhite) 
+
+    return (oldDrainList, tmpDrainList, oldDownList, tmpDownList, wrList, morgueList,srStatusList, fullSiteList, average_per_site)
+
+
+if __name__ == '__main__':
+    oldDrainList, tmpDrainList, oldDownList, tmpDownList, wrList, morgueList, srStatusList, fullSiteList, average_per_site = getAllInformation()
     
-    average_per_site = calculateSR(fullSiteList, daysGreen, daysYellow, daysBrown, daysWhite) 
-    
-    #_____________________________find drain and down sites_______________________________
-    
-    
-    #________________________________drain process_____________________________________
-    
-    for site in oldDrainList:
-        if not average_per_site[site] >= 0.8 : # if last week siteRanking > 80% remove from drainList
-            if not site in newDrainList: newDrainList.append(site)
+    for site in oldDrainList:                   # firstly add old drain list
+        if not average_per_site[site] >= 0.8 :  # if last week siteRanking > 80% remove from drainList
+            if not site in tmpDrainList: tmpDrainList.append(site)
 
     for site in fullSiteList:
-        if site in wrList: # add site into drainNewList if wr = in for site
-            if not site in newDrainList: newDrainList.append(site)
-        if site in srStatusList: # add site into drainNewList if srstatus = sd for site
-            if not site in newDrainList: newDrainList.append(site)
+        if site in wrList:                      # add site into drainNewList if wr = in for site
+            if not site in tmpDrainList: tmpDrainList.append(site)
+        if site in srStatusList:                # add site into drainNewList if srstatus = sd for site
+            if not site in tmpDrainList: tmpDrainList.append(site)
 
-    #________________________________________________________________________________
-
-    #________________________________down process_____________________________________
-
-    for site in oldDownList: 
-        if not site in newDownList: newDownList.append(site)
+    for site in oldDownList:                    # firstly add old down list 
+        if not site in tmpDownList: tmpDownList.append(site)
 
     for site in fullSiteList:
-        if site in morgueList: # add site into downNewList if morgue = in for site
-            if not site in newDownList: newDownList.append(site)
+        if site in morgueList:                  # add site into downNewList if morgue = in for site
+            if not site in tmpDownList: tmpDownList.append(site)
 
-    #________________________________________________________________________________
-            
-    #________________________________crate new list______________________________________
+    #________________________________ crate new list ______________________________________
     for site in fullSiteList:
-        if not site in newList:
-            if site in newDrainList:
-                newList[site] = "drain"
+        if not site in newDrainList:
+            if site in tmpDrainList:
+                newDrainList[site] = "drain"
             else:
-                newList[site] = "on"
+                newDrainList[site] = "on"
             
-            if site in newDownList:
-                newList[site] = "down"
+            if site in tmpDownList:
+                newDrainList[site] = "down"
                 
-    writeFile(newList) #write process
-    
-    
-    
-    
-    
-    
+    writeFile(newDrainList) # write process
