d3464604e601785dbbd3473291f28915c873ffe1
John P. Artieda <artiedaj@fnal.gov>
2014-07-23 11:54:46 -0500

    cd to location of scripts

--- SR_View_SSB/drain/run_drain.sh
+++ SR_View_SSB/drain/run_drain.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script finds which site should be production status = drain, down, on and creates 1 file which is drain.txt
+# Script finds which site should be production status = drain, down, on
+cd /afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/drain
 python drain.py &> drain_log.txt
 cp drain.txt /afs/cern.ch/user/c/cmst1/www/SST/
 cp drain_log.txt /afs/cern.ch/user/c/cmst1/www/SST/
