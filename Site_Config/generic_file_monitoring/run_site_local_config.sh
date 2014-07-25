0dc40a96fbdeb0f59005889b6cb3f6ef96a50923
John P. Artieda <artiedaj@fnal.gov>
2014-07-25 17:37:07 -0500

    removed emails option

--- Site_Config/generic_file_monitoring/run_site_local_config.sh
+++ Site_Config/generic_file_monitoring/run_site_local_config.sh
@@ -2,32 +2,8 @@
 # written by John Artieda
 
 # set up a proxy to read site config files
-#clear
 #source /afs/cern.ch/project/gd/LCG-share/new_3.2/etc/profile.d/grid_env.sh
 #voms-proxy-init -voms cms
-
-# Email if things are running slowly
-if [ -f scriptRunning.run ];
-then
-   echo "run_site_local_config.sh is already running. Will send an email to the admin."
-   # script to send simple email
-   # email subject
-   SUBJECT="[TFC] load site-local-config"
-   # Email To ?
-   EMAIL="cms-comp-ops-site-support-team@cern.ch"
-   # Email text/message
-   if [ -f emailmessage.txt ];
-   then
-      rm emailmessage.txt
-   fi
-   touch emailmessage.txt
-   echo "run_site_local_config.sh  is running slowly." > emailmessage.txt
-   # send an email using /bin/mail
-   /bin/mail -s "$SUBJECT" "$EMAIL" < emailmessage.txt
-else
-     echo "bash run_site_local_config.sh succesfully"
-     touch scriptRunning.run
-fi
 
 #Run the script
 path="/afs/cern.ch/user/j/jartieda/MonitoringScripts/Site_Config/generic_file_monitoring"
@@ -39,8 +15,6 @@ python $path"/"site_local_config.py $txt $findText &> $path"/"site_local_config.
 
 problem="$?"
 echo "problem: $problem"
-echo "The files were created succesfully."
 
 cp $txt".txt" /afs/cern.ch/user/c/cmst1/www/SST
-
-rm scriptRunning.run
+echo "The files were created succesfully."
--- Site_Config/phedex-node/run_phedex_node.sh
+++ Site_Config/phedex-node/run_phedex_node.sh
@@ -2,32 +2,8 @@
 # written by John Artieda
 
 # set up a proxy to read site config files
-#clear
 #source /afs/cern.ch/project/gd/LCG-share/new_3.2/etc/profile.d/grid_env.sh
 #voms-proxy-init -voms cms
-
-# Email if things are running slowly
-if [ -f scriptRunning.run ];
-then
-   echo "run_phedex_node.sh is already running. Will send an email to the admin."
-   # script to send simple email
-   # email subject
-   SUBJECT="[TFC] load phedex_node"
-   # Email To ?
-   EMAIL="cms-comp-ops-site-support-team@cern.ch"
-   # Email text/message
-   if [ -f emailmessage.txt ];
-   then
-      rm emailmessage.txt
-   fi
-   touch emailmessage.txt
-   echo "run_phedex_node.sh  is running slowly." > emailmessage.txt
-   # send an email using /bin/mail
-   /bin/mail -s "$SUBJECT" "$EMAIL" < emailmessage.txt
-else
-     echo "bash run_phedex_node.sh completed succesfully"
-     touch scriptRunning.run
-fi
 
 #Run the script
 path="/afs/cern.ch/user/j/jartieda/MonitoringScripts/Site_Config/phedex-node"
@@ -39,8 +15,6 @@ python $path"/"phedex_node.py $txt $findText &> $path"/"phedex_node.log
 
 problem="$?"
 echo "problem: $problem"
-echo "The files were created succesfully."
 
 cp $txt".txt" /afs/cern.ch/user/c/cmst1/www/SST/
-
-rm scriptRunning.run
+echo "The files were created succesfully."
