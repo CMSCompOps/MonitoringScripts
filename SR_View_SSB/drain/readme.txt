352fd6f75aadaa0868869b7707e40ef810954a5c
John P. Artieda <artiedaj@fnal.gov>
2014-07-29 14:11:04 -0500

    changes to read storage disk.xml

--- SR_View_SSB/drain/readme.txt
+++ SR_View_SSB/drain/readme.txt
@@ -1,11 +1,9 @@
-# Author : Gökhan Kandemir
-# E-mail : gokhan.kandemir@cern.ch
-# Script Responsible : Site Support Team
-# Script is located : /afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/drain
-# Outputs : 
-   # /afs/cern.ch/user/c/cmst1/www/SST/drain.txt (https://cmst1.web.cern.ch/CMST1/SST/drain.txt)
+# Script Responsible: Site Support Team
+# Script location: /afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/drain
+# Output: /afs/cern.ch/user/c/cmst1/www/SST/drain.txt (https://cmst1.web.cern.ch/CMST1/SST/drain.txt
+
 # The script is being run by an acronjob
-# The acronojb is in the acrontab of the user: cmst1
+# The acronjob is in the acrontab of the user: cmst1
 # 5,20,35,50 * * * * lxplus ssh vocms202 /afs/cern.ch/user/c/cmst1/scratch0/MonitoringScripts/SR_View_SSB/drain/run_drain.sh &> /dev/null
 # Description : # Script finds which site should be production status = drain, down, on and creates 1 file which is drain.txt [1]
 
--- Site_Config/xrootd-redirector/xrootd-redirector.py
+++ Site_Config/xrootd-redirector/xrootd-redirector.py
@@ -39,35 +39,50 @@ def fetch_all_sites(url, api):
     #______________________________________________________________________________________
     # To get all site-local-config information.
     for siteName in sitesList:
-        xml = getXmlfromURL('cmsweb.cern.ch','/gitweb/?p=siteconf/.git;a=blob_plain;f=' + siteName + '/PhEDEx/storage.xml;hb=HEAD')
+        xml = getXmlfromURL('cmsweb.cern.ch','/gitweb/?p=siteconf/.git;a=blob_plain;f=' + siteName + '/PhEDEx/storage.xml;hb=HEAD')   
         match(xml, siteName, findText1, findText2)
 #__________________________________________________________________________________________
 # function calculates the number of T1s and T2s counts and writes results to console and file.
 def match(xml, siteName, findText1, findText2):
-  global T1Count, T2Count, file, jsonCode
-  findTxt = "404 - Cannot find file"
-  saveTime = time.strftime('%Y-%m-%d %H:%M:%S')
-  url = "https://cmsweb.cern.ch/gitweb/?p=siteconf/.git;a=blob_plain;f=" + siteName + "/PhEDEx/storage.xml;hb=HEAD"
-  if (findTxt in xml) == False:     # site-local-config file found
-    dom = parseString(xml)
-    tag = dom.getElementsByTagName('lfn-to-pfn')
-    val = 0
-    for s in tag:
-        if (s.getAttribute('result') == findText1) or (s.getAttribute('result') == findText2):
-            val = 1
-    if val == 1:                    # findText found in file
-      print saveTime + "\t" + siteName + "\t" + "yes" + "\t" + "green" + "\t" + url
-      file.write(saveTime + "\t" + siteName + "\t" + "yes" + "\t" + "green" + "\t" + url + "\n")    
-    else:                           # findText not found in file
-      print saveTime + "\t" + siteName + "\t" + "no" + "\t" + "red" + "\t" + url
-      file.write(saveTime + "\t" + siteName + "\t" + "no" + "\t" + "red" + "\t" + url + "\n")
-      if (siteName[0:2] == "T1") : T1Count = T1Count + 1
-      if (siteName[0:2] == "T2") : T2Count = T2Count + 1 
-  else:                             # couldn't find site-local-config file
-    print saveTime + "\t" + siteName + "\t" + "no" + "\t" + "white" + "\t" + url
-    file.write(saveTime + "\t" + siteName + "\t" + "no" + "\t" + "white" + "\t" + url + "\n")
-    if (siteName[0:2] == "T1") : T1Count = T1Count + 1
-    if (siteName[0:2] == "T2") : T2Count = T2Count + 1
+    global T1Count, T2Count, file, jsonCode
+    findTxt = "404 - Cannot find file"
+    saveTime = time.strftime('%Y-%m-%d %H:%M:%S')
+    url = "https://cmsweb.cern.ch/gitweb/?p=siteconf/.git;a=blob_plain;f=" + siteName + "/PhEDEx/storage.xml;hb=HEAD"
+    n = 0
+    while n < 3:
+        if (findTxt in xml) == False:       # site-local-config file found
+            dom = parseString(xml)
+            tag = dom.getElementsByTagName('lfn-to-pfn')
+            val = 0
+            for s in tag:
+                if (s.getAttribute('result') == findText1) or (s.getAttribute('result') == findText2):
+                    val = 1
+            if val == 1:                    # findText found in file
+                n = 3
+                print saveTime + "\t" + siteName + "\t" + "yes" + "\t" + "green" + "\t" + url
+                file.write(saveTime + "\t" + siteName + "\t" + "yes" + "\t" + "green" + "\t" + url + "\n")    
+            else:                           # findText not found in file
+                n = n + 1
+                if n == 1:
+                    xml = getXmlfromURL('cmsweb.cern.ch','/gitweb/?p=siteconf/.git;a=blob_plain;f=' + siteName + '/PhEDEx/storage-disk.xml;hb=HEAD')
+                elif n == 2:
+                    xml = getXmlfromURL('cmsweb.cern.ch','/gitweb/?p=siteconf/.git;a=blob_plain;f=' + siteName + '/PhEDEx/storage_disk.xml;hb=HEAD')
+                else:
+                    print saveTime + "\t" + siteName + "\t" + "no" + "\t" + "red" + "\t" + url
+                    file.write(saveTime + "\t" + siteName + "\t" + "no" + "\t" + "red" + "\t" + url + "\n")
+                    if (siteName[0:2] == "T1") : T1Count = T1Count + 1
+                    if (siteName[0:2] == "T2") : T2Count = T2Count + 1 
+        else:                             # couldn't find site-local-config file
+            n = n + 1
+            if n == 1:
+                xml = getXmlfromURL('cmsweb.cern.ch','/gitweb/?p=siteconf/.git;a=blob_plain;f=' + siteName + '/PhEDEx/storage-disk.xml;hb=HEAD')
+            elif n == 2:
+                xml = getXmlfromURL('cmsweb.cern.ch','/gitweb/?p=siteconf/.git;a=blob_plain;f=' + siteName + '/PhEDEx/storage_disk.xml;hb=HEAD')
+            else:
+                print saveTime + "\t" + siteName + "\t" + "no" + "\t" + "white" + "\t" + url
+                file.write(saveTime + "\t" + siteName + "\t" + "no" + "\t" + "white" + "\t" + url + "\n")
+                if (siteName[0:2] == "T1") : T1Count = T1Count + 1
+                if (siteName[0:2] == "T2") : T2Count = T2Count + 1
 #_______________________________________________________________________________
 # function reads  your certificate and gets site-local-config.xml from URL
 def getXmlfromURL(url,api):
