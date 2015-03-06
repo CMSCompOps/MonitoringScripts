af92f8fe316aecbde809f1ba7ebd893e82e8b7a7
Mehmet <mehmet3@gmail.com>
2015-03-06 11:23:41 +0100

    move metricParser function (also change the name, metricParser -> parseMetric)

--- lib/dashboard.py
+++ lib/dashboard.py
@@ -1,6 +1,6 @@
 # aaltunda - ali.mehmet.altundag@cern.ch
 
-import time
+import time, re
 
 green  = 'green'
 yellow = 'yellow'
@@ -27,3 +27,20 @@ class entry:
 
     def __str__(self):
         return "%s\t%s\t%s\t%s\t%s" % (self.date, self.name, self.value, self.color, self.url)
+
+def parseMetric(data):
+    # remove python style comments
+    data    = re.sub(re.compile(r'#.*$', re.MULTILINE), "", data)
+    rows = re.findall(r'(.*?)\t(.*?)\t(.*?)\t(.*?)\t(.*?)\n', data, re.M)
+    ret  = []
+    # convert them into dashboard entry structure
+    for row in rows:
+        ret.append(entry(row[0], row[1], row[2], row[3], row[4]))
+    return ret
+
+if __name__ == '__main__':
+    import url
+    wrURL = 'https://cmst1.web.cern.ch/CMST1/WFMon/WaitingRoom_Sites.txt'
+    wr    = parseMetric(url.read(wrURL))
+    for site in wr:
+        print str(site)
