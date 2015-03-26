2eae5d4bac5010e5570180c29eb95be1d480feb9
Mehmet <mehmet3@gmail.com>
2015-03-26 17:11:37 +0100

    fix wrong metric bug

--- metrics/aaa/run.sh
+++ metrics/aaa/run.sh
@@ -16,6 +16,6 @@ fi
 samURL="http://wlcg-sam-cms.cern.ch/dashboard/request.py/getTestResults?profile_name=CMS_CRITICAL&metrics=org.cms.WN-xrootd-access%20(/cms/Role_lcgadmin)&hostname={0}&flavours=CREAM-CE,OSG-CE,ARC-CE,SRMv2&time_range=last2Weeks"
 
 # notice parameters in the following url
-hcURL="http://dashb-cms-job.cern.ch/dashboard/request.py/jobsummary-plot-or-table?activity=hcxrootd&date1={0}&date2={1}&sortby=site&scale=linear&check=terminated"
+hcURL="http://dashb-cms-job.cern.ch/dashboard/request.py/jobsummary-plot-or-table?activity=hcxrootd&date1={0}&date2={1}&sortby=inputse&scale=linear&check=terminated"
 
 python aaa.py $samURL $hcURL $OUT/aaa.txt
