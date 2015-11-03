7806295521c2e0018d0186a8110ffc51bf757299
Mehmet <mehmet3@gmail.com>
2015-11-03 16:46:38 +0100

    fix report bug

--- metrics/aaa/report.py
+++ metrics/aaa/report.py
@@ -1,6 +1,9 @@
 from lib import url, fileOps
-import sys, os, time
-try: import json
+import sys, os, time, copy
+try:
+    import json
+    from json import encoder
+    encoder.FLOAT_REPR = lambda o: format(o, '.2f')
 except ImportError: import simplejson as json
 
 if len(sys.argv) < 2:
@@ -11,44 +14,47 @@ tests = ['sam', 'hc', 'ggus']
 # set variables given from outside
 currentReport   = json.loads(fileOps.read(sys.argv[1]))
 htmlTemplate    = fileOps.read(sys.argv[2])
-finalReportFile = sys.argv[3]
+reportFile = sys.argv[3]
 htmlOutputPath  = sys.argv[4]
 
-if os.path.isfile(finalReportFile):
+if os.path.isfile(reportFile):
     print 'loading 2 weeks report...'
-    finalReport = json.loads(fileOps.read(finalReportFile))
+    report = json.loads(fileOps.read(reportFile))
 else:
     print 'creating new 2 weeks report...'
-    finalReport = {'data' : {}}
+    report = {'data' : {}}
 
 # insert new test results
 data = currentReport['data']
 for site in data:
-    if not finalReport['data'].has_key(site):
-        finalReport['data'][site] = {}
+    if not report['data'].has_key(site):
+        report['data'][site] = {}
     for test in tests:
-        if not finalReport['data'][site].has_key(test):
-            finalReport['data'][site][test] = {}
+        if not report['data'][site].has_key(test):
+            report['data'][site][test] = {}
         if not data[site].has_key(test):
             continue
-        finalReport['data'][site][test][currentReport['lastUpdate']] = data[site][test]
+        report['data'][site][test][currentReport['lastUpdate']] = data[site][test]
 
 # merge current report with old ones (delete test results older than 2 weeks!)
-data = finalReport['data']
-for site in data:
+tmpData = copy.deepcopy(report['data'])
+for site in tmpData:
     for test in tests:
-        for timeStamp in data[site][test]:
+        for timeStamp in tmpData[site][test]:
             # delete value if it is older than 2 weeks
             if time.time() - float(timeStamp) > 60*60*24*14:
-                del data[site][test][timeStamp]
+                del report['data'][site][test][timeStamp]
 
 # update time stamp
-finalReport['lastUpdate'] = time.time()
-finalReport = json.dumps(finalReport)
+report['lastUpdate'] = time.time()
+report['timeOffset'] = time.timezone
 
-htmlTemplate = htmlTemplate.replace('@report@', finalReport)
 htmlTemplate = htmlTemplate.replace('@date@', time.strftime("%Y-%m-%d at %H:%M:%S", time.localtime(time.time())))
 
 # write files and exit
-fileOps.write(finalReportFile, finalReport)
-fileOps.write('%s/report.html' % htmlOutputPath, htmlTemplate)
+fileOps.write(reportFile, json.dumps(report))
+for site in data:
+    siteReport = {'lastUpdate' : report['lastUpdate'], 'timeOffset' : report['timeOffset'],
+                  'data' : {site : report['data'][site]}}
+    siteReport = htmlTemplate.replace('@report@', json.dumps(siteReport))
+    fileOps.write('%s/%s_report.html' % (htmlOutputPath, site), siteReport)
