42f005b378e757880051f36c97dd57bdcee1d41a
Mehmet <mehmet3@gmail.com>
2015-12-17 21:48:49 +0100

    remove scripts that we are not responsible for

--- SiteComm/DDT/phedex_monitor/service/dummy_datasource.py
+++
@@ -1,8 +0,0 @@
-
-import datetime, cPickle
-
-class DummyDatasource:
-    
-    def read_from_file(self, filename):
-        return cPickle.load( filename )      
-
--- SiteComm/SSBScripts/EnabledLinksForSiteMonitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pushd . > /dev/null
-cd /tmp
-$HOME/COMP/SITECOMM/SSBScripts/EnabledLinksForSiteMonitor.py
-mv EnabledLinksForSiteMonitor.txt  /afs/cern.ch/cms/LCG/SiteComm/
-mv EnabledLinksStatus.html   /afs/cern.ch/cms/LCG/SiteComm/
-popd > /dev/null
-
