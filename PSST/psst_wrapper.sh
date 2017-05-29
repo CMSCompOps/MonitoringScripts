1fa3c888b8e1530d804df5a5ebfae162ecf29a6a
roksys <rokas.ma@gmail.com>
2017-05-29 16:04:11 +0200

    Fix for running locally and paths

--- PSST/psst_wrapper.sh
+++ PSST/psst_wrapper.sh
@@ -7,14 +7,15 @@
 glidein_config="$1"
 time_limit=300
 
-timeout $time_limit ./psst.sh $glidein_config
+my_tar_dir=`grep -m1 -i '^GLIDECLIENT_CMS_PSST' $glidein_config | awk '{print $2}'`
+echo $my_tar_dir/psst.sh
+timeout $time_limit ${my_tar_dir}/psst.sh $glidein_config
 
 exit_code=$?
 
-site_name=`grep -m1 -i '^GLIDEIN_CMSSite' $glidein_config`
-
-if [ $exit_code = 124 ];then
+if [ "$exit_code" -ge 124 -a "$exit_code" -le 137 ];then
     echo "PSST reached time limit - ${time_limit} s"
+    site_name=`grep -m1 -i '^GLIDEIN_CMSSite' $glidein_config | awk '{print $2}'`
     hostname | /usr/bin/Mail -s "PSST reached time limit - ${time_limit} s at ${site_name}" rokas.maciulaitis@cern.ch
     exit $exit_code
 else
