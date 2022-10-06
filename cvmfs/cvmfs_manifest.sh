179bdea0ff0cadc1e5cdd9eb764577eb8ced5a8a
Stephan Lammel <lammel@fnal.gov>
2022-10-06 16:51:02 -0500

    add oasis repository for OSG sites

--- cvmfs/cvmfs_manifest.sh
+++ cvmfs/cvmfs_manifest.sh
@@ -7,19 +7,25 @@
 #                                                                             #
 # 2022-Aug-30 Stephan Lammel                                                  #
 # #############################################################################
-URL="http://cvmfs-stratum-zero.cern.ch/cvmfs/cms.cern.ch/.cvmfspublished"
-DIR="/eos/user/c/cmssst/www/cvmfs/cms.cern.ch"
+URLS="http://cvmfs-stratum-zero.cern.ch/cvmfs/cms.cern.ch/.cvmfspublished http://oasis.opensciencegrid.org:8000/cvmfs/oasis.opensciencegrid.org/.cvmfspublished"
+DIRS="/eos/user/c/cmssst/www/cvmfs/"
 #
 umask 022
+EXITCODE=0
 
 
 
 null_cvmfspublished()
 {
-   ERROR_MSG=$1
-   /bin/rm -f ${DIR}/new.cvmfspublished
-   echo -e -n "C0000000000000000000000000000000000000000\nB0\nRd41d8cd98f00b204e9800998ecf8427e\nD0\nS0\nGno\nAno\nNcms.cern.ch\nX0000000000000000000000000000000000000000\nH0000000000000000000000000000000000000000\nT0\nM0000000000000000000000000000000000000000\nY0000000000000000000000000000000000000000\n--\n00000000deadbeef00000000deadbeef00000000\n${ERROR_MSG} !!!" 1>${DIR}/new.cvmfspublished 2>/dev/null
-   /bin/mv ${DIR}/new.cvmfspublished ${DIR}/.cvmfspublished
+   MY_AREA=$1
+   if [ ${MY_AREA} = "" -o [ ${MY_AREA} = *" "* ]]; then
+      return
+   fi
+   MY_REPO=`echo "${MY_AREA}" | /usr/bin/awk -F/ '{print $NF;exit}'`
+   ERROR_MSG=$2
+   /bin/rm -f ${MY_AREA}/new.cvmfspublished
+   echo -e -n "C0000000000000000000000000000000000000000\nB0\nRd41d8cd98f00b204e9800998ecf8427e\nD0\nS0\nGno\nAno\nN${MY_REPO}\nX0000000000000000000000000000000000000000\nH0000000000000000000000000000000000000000\nT0\nM0000000000000000000000000000000000000000\nY0000000000000000000000000000000000000000\n--\n00000000deadbeef00000000deadbeef00000000\n${ERROR_MSG} !!!" 1>${MY_AREA}/new.cvmfspublished 2>/dev/null
+   /bin/mv ${MY_AREA}/new.cvmfspublished ${MY_AREA}/.cvmfspublished
 }
 
 
@@ -36,29 +42,46 @@ fi
 
 /usr/bin/eosfusebind -g
 
-/usr/bin/timeout 90 /usr/bin/wget -O ${DIR}/new.cvmfspublished -T 60 ${URL}
-RC=$?
-if [ ${RC} -eq 124 ]; then
-   null_cvmfspublished "timeout contacting Stratum-0"
-   exit 1
-elif [ ${RC} -ne 0 ]; then
-   null_cvmfspublished "failed to fetch Stratum-0 manifest"
-   exit 1
-fi
 
-NOW=`/usr/bin/date +"%s"`
-TSP=`/usr/bin/awk -v now=${NOW} 'BEGIN{t=now} $0 ~ /^T/ {t=int(substr($0,2))} END{print int(now-t)}' ${DIR}/new.cvmfspublished`
-TDL=`/usr/bin/awk -v tsp=${TSP} 'BEGIN{t=900} $0 ~ /^D/ {t=2*int(substr($0,2))} END{print int(t-tsp)}' ${DIR}/new.cvmfspublished`
-if [ ${TDL} -gt 0 ]; then
-   echo "sleeping ${TDL} seconds before update"
-   /bin/sleep ${TDL}
-fi
 
-/bin/mv ${DIR}/new.cvmfspublished ${DIR}/.cvmfspublished
-RC=$?
+# fetch manifest for repositories:
+for URL in ${URLS}; do
+   REPO=`echo "${URL}" | /usr/bin/awk -F/ '{print $(NF-1);exit}'`
+   AREA=${DIRS}/${REPO}
 
-if [ ! -e ${DIR}/.htaccess ]; then
-   echo -e "<Files \".cvmfspublished\">\n   Header set Cache-Control \"public, max-age=3600\"\n   Header set Access-Control-Max-Age 3600\n</Files>" 1> ${DIR}/.htaccess 2>/dev/null
-fi
+   /usr/bin/timeout 90 /usr/bin/wget -O ${AREA}/new.cvmfspublished -T 60 ${URL}
+   RC=$?
+   if [ ${RC} -eq 124 ]; then
+      null_cvmfspublished "${AREA}" "timeout contacting Stratum-0"
+      if [ ${EXITCODE} -eq 0 ]; then
+         EXITCODE=1
+      fi
+      continue
+   elif [ ${RC} -ne 0 ]; then
+      null_cvmfspublished "${AREA}" "failed to fetch Stratum-0 manifest"
+      if [ ${EXITCODE} -eq 0 ]; then
+         EXITCODE=1
+      fi
+      continue
+   fi
 
-exit ${RC}
+   NOW=`/usr/bin/date +"%s"`
+   TSP=`/usr/bin/awk -v now=${NOW} 'BEGIN{t=now} $0 ~ /^T/ {t=int(substr($0,2))} END{print int(now-t)}' ${AREA}/new.cvmfspublished`
+   TDL=`/usr/bin/awk -v tsp=${TSP} 'BEGIN{t=900} $0 ~ /^D/ {t=2*int(substr($0,2))} END{print int(t-tsp)}' ${AREA}/new.cvmfspublished`
+   if [ ${TDL} -gt 0 ]; then
+      echo "sleeping ${TDL} seconds before update"
+      /bin/sleep ${TDL}
+   fi
+
+   /bin/mv ${AREA}/new.cvmfspublished ${AREA}/.cvmfspublished
+   RC=$?
+   if [ ${EXITCODE} -eq 0 ]; then
+      EXITCODE=${RC}
+   fi
+
+   if [ ! -e ${AREA}/.htaccess ]; then
+      echo -e "<Files \".cvmfspublished\">\n   Header set Cache-Control \"public, max-age=3600\"\n   Header set Access-Control-Max-Age 3600\n</Files>" 1> ${AREA}/.htaccess 2>/dev/null
+   fi
+done
+
+exit ${EXITCODE}
