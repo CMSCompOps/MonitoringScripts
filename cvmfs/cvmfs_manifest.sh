3b2c0a9501203f3ed18d2a93b40ecf0341c25c1a
Stephan Lammel <lammel@fnal.gov>
2022-10-24 03:03:17 -0500

    add singularity.opensciencegrid.org manifest fetching

--- cvmfs/cvmfs_manifest.sh
+++ cvmfs/cvmfs_manifest.sh
@@ -7,7 +7,7 @@
 #                                                                             #
 # 2022-Aug-30 Stephan Lammel                                                  #
 # #############################################################################
-URLS="http://cvmfs-stratum-zero.cern.ch/cvmfs/cms.cern.ch/.cvmfspublished http://oasis.opensciencegrid.org:8000/cvmfs/oasis.opensciencegrid.org/.cvmfspublished"
+URLS="http://cvmfs-stratum-zero.cern.ch/cvmfs/cms.cern.ch/.cvmfspublished http://oasis.opensciencegrid.org:8000/cvmfs/oasis.opensciencegrid.org/.cvmfspublished http://oasis-replica.opensciencegrid.org:8000/cvmfs/singularity.opensciencegrid.org/.cvmfspublished"
 DIRS="/eos/user/c/cmssst/www/cvmfs/"
 #
 umask 022
